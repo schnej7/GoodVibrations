69f67270649fd3e1d07338eb3f41e98b813dc9b0
Hans Vorsteveld <hvorsteveld@gmail.com>
2012-03-19 00:08:48 -0400

    Adding sample implementations of trigger functions

---
+++ src/teamwork/goodVibrations/functions/RaiseFunction.java
@@ -0,0 +1,20 @@
+package teamwork.goodVibrations.functions;
+
+import android.media.AudioManager;
+import android.util.Log;
+
+public class RaiseFunction implements Function {
+
+	private AudioManager AM;
+	
+	public void execute() {
+		AM.adjustStreamVolume(AudioManager.STREAM_RING, AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
+		Log.d("vorsth","Addtion Function ");
+	}
+	
+	public RaiseFunction(AudioManager am)
+	{
+		AM = am;
+	}
+
+}
