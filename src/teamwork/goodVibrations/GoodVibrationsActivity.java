6354be8a09b8e187ac3962206e83af71258abae6
Hans Vorsteveld <hvorsteveld@gmail.com>
2012-03-19 00:13:04 -0400

    Made service run in its own thread

--- src/teamwork/goodVibrations/GoodVibrationsActivity.java
+++ src/teamwork/goodVibrations/GoodVibrationsActivity.java
@@ -1,10 +1,12 @@
 package teamwork.goodVibrations;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 public class GoodVibrationsActivity extends Activity
 {
