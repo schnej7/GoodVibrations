5d29c5e4db407df116a73f9cf9a90a3f716619e5
Hans Vorsteveld <hvorsteveld@gmail.com>
2012-03-21 13:32:54 -0400

    Simplify and change Trigger.java to an interface

--- src/teamwork/goodVibrations/triggers/Trigger.java
+++ src/teamwork/goodVibrations/triggers/Trigger.java
@@ -1,38 +1,10 @@
 package teamwork.goodVibrations.triggers;
 
-import java.util.ArrayList;
+import teamwork.goodVibrations.functions.Function;
 
-public class Trigger
+public interface Trigger
 {
-	private long pollInterval;
-	private long nextPoll;
-	private ArrayList<Integer> functionIDs;
-	public String name;
-	
-	public Trigger(String n, int t)
-	{
-		name = n;
-		pollInterval = t;
-	}
-	
-	public long getPollInterval()
-	{
-		return pollInterval;
-	}
-	
-	public long getNextPoll()
-	{
-		return nextPoll;
-	}
-	
-	public void setNextPoll(long p)
-	{
-		nextPoll = p;
-	}
-	
-	@Override
-	public String toString()
-	{
-		return super.toString() + ": " + name + ", " + nextPoll;
-	}
+  boolean addFunction(Function f);
+  void removeFunction();
+  void executeFunctions();
 }
