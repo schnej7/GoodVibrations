7a218afc0dcb3de5913ef1e7c4b3c135de63394e
eliztowns <townse2@rpi.edu>
2012-03-21 13:58:08 -0400

    time trigger

--- src/teamwork/goodVibrations/triggers/TimeTrigger.java
+++ src/teamwork/goodVibrations/triggers/TimeTrigger.java
@@ -1,26 +1,47 @@
 package teamwork.goodVibrations.triggers;
 
+import java.util.Calendar;
+
 import teamwork.goodVibrations.functions.Function;
 
 public class TimeTrigger implements Trigger
 {
+    private Calendar begin;
+    private Calendar end;
+    
   
-public TimeTrigger()
-{
-}
-
-public boolean addFunction(Function f)
-{
-  return true;
-}
-
-public void removeFunction()
-{
- 
-}
-
-public void executeFunctions()
-{
+  public TimeTrigger()
+  {
+  }
   
-}
+  
+  public boolean addFunction(Function f)
+  {
+    return true;
+  }
+  
+  public void removeFunction()
+  {
+  }
+  
+  public void executeFunctions()
+  { 
+  }
+  
+  public void setBegin(Calendar c)
+  {
+    begin=c;
+  }
+  public void setEnd(Calendar c)
+  {
+    begin=c;
+  }
+  public Calendar getBegin()
+  {
+    return begin;
+  }
+  public Calendar getEnd()
+  {
+    return end;
+  }
 }
