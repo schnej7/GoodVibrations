51541da883a72df1aa4acd8f15963870c29922b3
eliztowns <townse2@rpi.edu>
2012-03-21 13:36:58 -0400

    TIME TRIGGER PRO-NESS

--- src/teamwork/goodVibrations/triggers/TimeTrigger.java
+++ src/teamwork/goodVibrations/triggers/TimeTrigger.java
@@ -1,10 +1,26 @@
 package teamwork.goodVibrations.triggers;
 
-public class TimeTrigger extends Trigger
+import teamwork.goodVibrations.functions.Function;
+
+public class TimeTrigger implements Trigger
 {
   
 public TimeTrigger()
 {
 }
 
+public boolean addFunction(Function f)
+{
+  return true;
+}
+
+public void removeFunction()
+{
+ 
+}
+
+public void executeFunctions()
+{
+  
+}
 }
