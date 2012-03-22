a56e27bc044a8751ae881ae2daf868176b50f4d6
schnej7 <schnej7@rpi.edu>
2012-03-22 17:25:46 -0400

    added classes for tabbed view

---
+++ src/teamwork/goodVibrations/FunctionDisplayActivity.java
@@ -0,0 +1,11 @@
+package teamwork.goodVibrations;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FunctionDisplayActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.testview1);
+    }
+}
---
+++ src/teamwork/goodVibrations/TriggerDisplayActivity.java
@@ -0,0 +1,11 @@
+package teamwork.goodVibrations;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TriggerDisplayActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.testview0);
+    }
+}
