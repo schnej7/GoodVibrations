ed74848b828728190c8b3e2322683343f074a008
Hans Vorsteveld <hvorsteveld@gmail.com>
2012-03-24 09:21:51 -0400

    Updated to most recent version

--- src/teamwork/goodVibrations/TriggerQueue.java
+++ src/teamwork/goodVibrations/TriggerQueue.java
@@ -2,86 +2,52 @@ package teamwork.goodVibrations;
 
 import java.util.Comparator;
 import java.util.PriorityQueue;
-import java.util.Set;
-
 import android.util.Log;
 
 import teamwork.goodVibrations.triggers.Trigger;
 
 public class TriggerQueue
 {
-  /*
 	public PriorityQueue<Trigger> q;
-	private Set<Trigger> triggers;
+	private Comparator<Trigger> comparator;
 	
-	public TriggerQueue(Set<Trigger> set)
+	public TriggerQueue()
 	{
-		triggers = set;
-		q = new PriorityQueue<Trigger>(triggers.size(), new Comparator<Trigger>()
-		{
-			public int compare(Trigger t1, Trigger t2)
-			{
-				if(t1.getNextPoll() == t2.getNextPoll())
-					return 0;
-				return t1.getNextPoll() < t2.getNextPoll() ? -1 : 1;
-			}
-		});
+	  comparator = new Comparator<Trigger>()
+	  {
+      public int compare(Trigger t1, Trigger t2)
+      {
+        if(t1.getNextExecutionTime() == t2.getNextExecutionTime())
+        {
+          return 0;
+        }
+        return t1.getNextExecutionTime() < t2.getNextExecutionTime() ? -1 : 1;
+      }
+    };
+		q = new PriorityQueue<Trigger>(1, comparator);
 	}
-	public int getSleepTimeMilli()
+	
+	public void push(Trigger t)
 	{
-		Trigger t = q.peek();
-		long ret = t.getNextPoll() - System.nanoTime();
-		return (int) (ret < 0 ? 0 : ret/1000000);
+	  q.add(t);
 	}
+	
 	public Trigger pop()
 	{
-      try
+    try
 	  {
-        Trigger t = q.poll();
-        t.setNextPoll(t.getNextPoll() + t.getPollInterval()*1000000);
-        q.add(t);
-        return t;
-      }
-      catch(Exception e)
-      {
-        Log.w("vorsth","Queue is empty");
-        return null;
-      }
-	}
-	
-	public void finishTriggerExecution()
-	{
-		try
-		{
-		  q.poll();
-		}
-		catch(NullPointerException e)
-		{
-		  Log.w("vorsth","Queue is empty");
-		}
-	}
-	
-	public void initialize()
-	{
-		long n = System.nanoTime();
-		for(Trigger t : triggers)
-		{
-			t.setNextPoll(n);
-			q.add(t);
-		}
-		triggers = null;
+      return q.poll();
+    }
+    catch(Exception e)
+    {
+      Log.w("vorsth","Queue is empty");
+      return null;
+    }
 	}
 	
 	public int size()
 	{
-		int s = q.size();
-		return s;
+		return q.size();
 	}
-	*/
-	//TODO Implement this
-	public void push(Trigger a_trigger) {}
-	//TODO Implement this
-	public Trigger pop(){return null;}
-	
-	
 }
+
