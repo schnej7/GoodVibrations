35704474e6991c026fc6cf83980c4575d07263ee
Hans Vorsteveld <hvorsteveld@gmail.com>
2012-03-25 01:12:32 -0400

    Fix some formatting

--- src/teamwork/goodVibrations/TriggerQueue.java
+++ src/teamwork/goodVibrations/TriggerQueue.java
@@ -8,13 +8,13 @@ import teamwork.goodVibrations.triggers.Trigger;
 
 public class TriggerQueue
 {
-	public PriorityQueue<Trigger> q;
-	private Comparator<Trigger> comparator;
-	
-	public TriggerQueue()
-	{
-	  comparator = new Comparator<Trigger>()
-	  {
+  public PriorityQueue<Trigger> q;
+  private Comparator<Trigger> comparator;
+
+  public TriggerQueue()
+  {
+    comparator = new Comparator<Trigger>()
+        {
       public int compare(Trigger t1, Trigger t2)
       {
         if(t1.getNextExecutionTime() == t2.getNextExecutionTime())
@@ -23,19 +23,19 @@ public class TriggerQueue
         }
         return t1.getNextExecutionTime() < t2.getNextExecutionTime() ? -1 : 1;
       }
-    };
-		q = new PriorityQueue<Trigger>(1, comparator);
-	}
-	
-	public void push(Trigger t)
-	{
-	  q.add(t);
-	}
-	
-	public Trigger pop()
-	{
+        };
+        q = new PriorityQueue<Trigger>(1, comparator);
+  }
+
+  public void push(Trigger t)
+  {
+    q.add(t);
+  }
+
+  public Trigger pop()
+  {
     try
-	  {
+    {
       return q.poll();
     }
     catch(Exception e)
@@ -43,11 +43,11 @@ public class TriggerQueue
       Log.w("vorsth","Queue is empty");
       return null;
     }
-	}
-	
-	public int size()
-	{
-		return q.size();
-	}
+  }
+
+  public int size()
+  {
+    return q.size();
+  }
 }
 
