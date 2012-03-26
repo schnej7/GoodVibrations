634551043c48ae096aab6363c3395ae9751a92af
Hans Vorsteveld <hvorsteveld@gmail.com>
2012-03-26 19:36:54 -0400

    Add Constants and Uitl Files

---
+++ src/teamwork/goodVibrations/Constants.java
@@ -0,0 +1,15 @@
+package teamwork.goodVibrations;
+
+public class Constants
+{
+  // TODO CHANGE BACK TO A FULL DAY
+  //public static long dayInMillis = 86400000;
+  public static final long dayInMillis = 30000;
+  public static final int FUNCTION_TYPE_VOLUME = 0;
+  public static final int FUNCTION_TYPE_RINGTONE = 1;
+  public static final String INTENT_KEY_TYPE = "keytype1";
+  public static final String INTENT_KEY_NAME = "keyname1";
+  public static final String INTENT_KEY_VOLUME = "KEYVOL";
+  public static final String INTENT_KEY_VIBRATE = "KEYVIBE";
+  public static final String INTENT_BUNDLE = "BUNDLE";
+}
