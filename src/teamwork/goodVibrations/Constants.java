64d288937391a33ac9fa0ef34dd32a0ec7bc0e15
Hans Vorsteveld <hvorsteveld@gmail.com>
2012-03-26 23:11:12 -0400

    Added constants so than intents passed to the service can be differentiated

--- src/teamwork/goodVibrations/Constants.java
+++ src/teamwork/goodVibrations/Constants.java
@@ -5,11 +5,17 @@ public class Constants
   // TODO CHANGE BACK TO A FULL DAY
   //public static long dayInMillis = 86400000;
   public static final long dayInMillis = 30000;
+  public static final String INTENT_TYPE = "INTENT_TYPE";
+  public static final int TRIGGER_TYPE = 0;
+  public static final int FUNCTION_TYPE = 1;
   public static final int FUNCTION_TYPE_VOLUME = 0;
   public static final int FUNCTION_TYPE_RINGTONE = 1;
+  public static final int TRIGGER_TYPE_TIME = 0;
+  public static final int TRIGGER_TYPE_LOCATION = 1;
   public static final String INTENT_KEY_TYPE = "keytype1";
   public static final String INTENT_KEY_NAME = "keyname1";
   public static final String INTENT_KEY_VOLUME = "KEYVOL";
   public static final String INTENT_KEY_VIBRATE = "KEYVIBE";
   public static final String INTENT_BUNDLE = "BUNDLE";
+  
 }
