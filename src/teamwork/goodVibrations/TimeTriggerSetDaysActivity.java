0b0aefb50d18dbe14711ddd6637ee190a4870088
schnej7 <schnej7@rpi.edu>
2012-03-28 13:54:07 -0400

    Added check boxes for days of week

--- src/teamwork/goodVibrations/TimeTriggerSetDaysActivity.java
+++ src/teamwork/goodVibrations/TimeTriggerSetDaysActivity.java
@@ -2,42 +2,34 @@ package teamwork.goodVibrations;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.CheckBox;
 
 public class TimeTriggerSetDaysActivity extends Activity
 {
-  private static final String TAG = "TimeTriggerSetDaysActivity";
-  Intent mIntent;
+  Intent mIntent = new Intent();
 
   protected void onStart(){
     final Button buttonDone = (Button)findViewById(R.id.buttonTimeTriggerDone);
+    final CheckBox chkSunday = (CheckBox) findViewById(R.id.chkSunday);
+    final CheckBox chkMonday = (CheckBox) findViewById(R.id.chkMonday);
+    final CheckBox chkTuesday = (CheckBox) findViewById(R.id.chkTuesday);
+    final CheckBox chkWednesday = (CheckBox) findViewById(R.id.chkWednesday);
+    final CheckBox chkThursday = (CheckBox) findViewById(R.id.chkThursday);
+    final CheckBox chkFriday = (CheckBox) findViewById(R.id.chkFriday);
+    final CheckBox chkSaturday = (CheckBox) findViewById(R.id.chkSaturday);
+    
     buttonDone.setOnClickListener(new View.OnClickListener()
     {
       public void onClick(View v)
       {
-        //sets the name in the intent
-        mIntent.putExtra(Constants.INTENT_KEY_NAME, txtName.getText().toString());
-        //start
+        byte days = 0;
+        //days = (byte) (days | 1);
+        if (chkSunday.isChecked()) days |= 
         setResult(RESULT_OK, mIntent);
         finish();  // Returns to FunctionDisplayActivity.onActivityResult()
       }
     }); 
-  }
-  
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data){
-    super.onActivityResult(requestCode, resultCode, data);
-    if(resultCode==RESULT_OK)
-    {
-      // If the ring tone picker was returned
-    }
-    else
-    {
-      Log.d(TAG, "RINGTONE RESULT FAIL");
-      Toast.makeText(this, "Ringtone Fail", Toast.LENGTH_LONG).show();
-    }
   }
 }
