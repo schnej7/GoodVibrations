85ec3b320b0ad3e8d2237a8f8f50681ae6012e6a
zettet <tzettersten@gmail.com>
2012-03-28 13:44:58 -0400

    Committed intent

--- src/teamwork/goodVibrations/TriggerDisplayActivity.java
+++ src/teamwork/goodVibrations/TriggerDisplayActivity.java
@@ -1,6 +1,7 @@
 package teamwork.goodVibrations;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
