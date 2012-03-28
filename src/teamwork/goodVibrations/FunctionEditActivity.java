2f848adda89d9bff0f0eb10c1538169cd460303b
Hans Vorsteveld <hvorsteveld@gmail.com>
2012-03-28 00:15:47 -0400

    Sends ringtone infromation bact to the FunctionDispalyActivity.onActivityResult().  Added a checkbox for vibrate with the ringtone function

--- src/teamwork/goodVibrations/FunctionEditActivity.java
+++ src/teamwork/goodVibrations/FunctionEditActivity.java
@@ -1,8 +1,5 @@
 package teamwork.goodVibrations;
 
-import java.util.Iterator;
-import java.util.Set;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -26,6 +23,7 @@ public class FunctionEditActivity extends Activity{
 	
 	private static final String TAG = "FunctionEditActivity";
 	Intent mIntent;
+	Uri ringtone_uri;
 	
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -48,7 +46,8 @@ public class FunctionEditActivity extends Activity{
 		final LinearLayout llRingtoneOptions = (LinearLayout) findViewById(R.id.llRingTone);
 		
 		final SeekBar sliderVolume = (SeekBar) findViewById(R.id.skbarVolume);
-		final CheckBox chkVibrate = (CheckBox) findViewById(R.id.chkVibrate);
+		final CheckBox chkVolumeVibrate = (CheckBox) findViewById(R.id.chkVolumeVibrate);
+		final CheckBox chkRingtoneVibrate = (CheckBox) findViewById(R.id.chkRingtoneVibrate);
 		final EditText txtName = (EditText) findViewById(R.id.editTextFunctionName);
 		
 		final Spinner spinnerType = (Spinner) findViewById(R.id.typeSelect);
@@ -77,14 +76,16 @@ public class FunctionEditActivity extends Activity{
 		    }
 		});
 		
+		// The Select Ringtone button
 		final Button buttonSelectRingtone = (Button)findViewById(R.id.buttonSelectRingtone);
 		buttonSelectRingtone.setOnClickListener( new View.OnClickListener()
     {
+		  // Start the ringtone picker on click and then get the result in the onActivityResult funciton below.
       public void onClick(View v)
       {
         Intent i = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
 
-        startActivityForResult(i, 1); 
+        startActivityForResult(i, Constants.REQUEST_CODE_RINGTONE_PICKER); 
       }
     });
 		
@@ -96,40 +97,48 @@ public class FunctionEditActivity extends Activity{
         	int i = spinnerType.getSelectedItemPosition();
         	mIntent.putExtra(Constants.INTENT_KEY_TYPE, i);
         	mIntent.putExtra(Constants.INTENT_KEY_NAME, txtName.getText().toString());
+        	mIntent.putExtra(Constants.INTENT_TYPE, Constants.FUNCTION_TYPE);
         	switch(i)
         	{
-		        case 0:
+		        case Constants.FUNCTION_TYPE_VOLUME:
 		        	// Use the volume fields
 		          // Convert 0-99 volume to 0 to MaxVol
 		          float progress = (float)sliderVolume.getProgress();
 		          float maxVol = (float)((AudioManager)getBaseContext().getSystemService(Context.AUDIO_SERVICE)).getStreamMaxVolume(AudioManager.STREAM_RING);
 		          int volume = (int) Math.round(maxVol*(progress/100.0));
-		          mIntent.putExtra(Constants.INTENT_TYPE, Constants.FUNCTION_TYPE);
+		          mIntent.putExtra(Constants.INTENT_KEY_TYPE, Constants.FUNCTION_TYPE_VOLUME);
 		        	mIntent.putExtra(Constants.INTENT_KEY_VOLUME, volume);
-		        	mIntent.putExtra(Constants.INTENT_KEY_VIBRATE, chkVibrate.isChecked());
+		        	mIntent.putExtra(Constants.INTENT_KEY_VIBRATE, chkVolumeVibrate.isChecked());
 		        	break;
-		        case 1:
-		        	//Use the tone fields
+		        case Constants.FUNCTION_TYPE_RINGTONE:
+		        	//Use the ringtone fields
 		          mIntent.putExtra(Constants.INTENT_KEY_TYPE, Constants.FUNCTION_TYPE_RINGTONE);
+		          mIntent.putExtra(Constants.INTENT_KEY_URI, ringtone_uri);
+		          mIntent.putExtra(Constants.INTENT_KEY_VIBRATE, chkRingtoneVibrate.isChecked());
 		        	break;
 		        default:
 		        	//Do nothing, this should never happen
 		        	break;
 	        }
         	setResult(RESULT_OK, mIntent);
-        	finish();
+        	finish();  // Returns to FunctionDisplayActivity.onActivityResult()
         }
     });
 	}
 	
  @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+  protected void onActivityResult(int requestCode, int resultCode, Intent data)
+  {
     super.onActivityResult(requestCode, resultCode, data);
     if(resultCode==RESULT_OK)
     {
-      Uri uri = data.getParcelableExtra(RingtoneManager.EXTRA_RINGTONE_PICKED_URI); 
-      Log.d(TAG, "uri " + uri);
-      Toast.makeText(this, "" + uri, Toast.LENGTH_LONG).show();
+      // If the ringtone picker was returned
+      if(requestCode == Constants.REQUEST_CODE_RINGTONE_PICKER)
+      {
+        ringtone_uri = data.getParcelableExtra(RingtoneManager.EXTRA_RINGTONE_PICKED_URI); 
+        Log.d(TAG, "uri " + ringtone_uri);
+        Toast.makeText(this, "" + ringtone_uri, Toast.LENGTH_LONG).show(); 
+      }
     }
     else
     {
