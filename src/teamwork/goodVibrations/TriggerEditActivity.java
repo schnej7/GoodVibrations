e56de3157398b6152eb78a53d371e080de118ae7
Hans Vorsteveld <hvorsteveld@gmail.com>
2012-03-29 01:39:42 -0400

    Drops into AddLocationTrigger menu

--- src/teamwork/goodVibrations/TriggerEditActivity.java
+++ src/teamwork/goodVibrations/TriggerEditActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class TriggerEditActivity extends Activity
 {
@@ -26,11 +25,20 @@ public class TriggerEditActivity extends Activity
     final Button buttonAddTimeTrigger = (Button) findViewById(R.id.buttonAddTimeTrigger);
     buttonAddTimeTrigger.setOnClickListener(new View.OnClickListener()
     {
-      
       public void onClick(View v)
       {
         Intent TimeTriggerEditIntent = new Intent(getApplicationContext(), TimeTriggerEditActivity.class);
-        startActivityForResult(TimeTriggerEditIntent,0);
+        startActivityForResult(TimeTriggerEditIntent,Constants.REQUEST_CODE_TIME);
+      }
+    });
+    
+    final Button buttonAddLocationTrigger = (Button) findViewById(R.id.buttonAddLocTrigger);
+    buttonAddLocationTrigger.setOnClickListener(new View.OnClickListener()
+    {
+      public void onClick(View v)
+      {
+        Intent LocationTriggerEditIntent = new Intent(getApplicationContext(), LocationTriggerEditActivity.class);
+        startActivityForResult(LocationTriggerEditIntent,Constants.REQUEST_CODE_LOCATION);
       }
     });
   }
@@ -39,16 +47,27 @@ public class TriggerEditActivity extends Activity
   protected void onActivityResult(int requestCode, int resultCode, Intent data)
   {
     super.onActivityResult(requestCode, resultCode, data);
+    Log.d(TAG,"onActivityResult()");
     if(resultCode==RESULT_OK)
     {
+      Bundle b = data.getExtras();
+      switch(requestCode)
+      {
+        case Constants.REQUEST_CODE_TIME:
+          Log.d(TAG,"ST: " + b.getLong(Constants.INTENT_KEY_START_TIME));
+          break;
+        
+        case Constants.REQUEST_CODE_LOCATION:
+          
+          break;
+      }
       // If the ring tone picker was returned
       setResult(RESULT_OK, data);
       finish();  // Returns to FunctionDisplayActivity.onActivityResult()
     }
     else
     {
-      Log.d(TAG, "RINGTONE RESULT FAIL");
-      Toast.makeText(this, "Ringtone Fail", Toast.LENGTH_LONG).show();
+      Log.d(TAG, "onActivityResult Failed");
     }
   }
 }
