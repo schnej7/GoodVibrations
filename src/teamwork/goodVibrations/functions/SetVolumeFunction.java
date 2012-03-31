f03eb00a9885cfb0a20fa8ebb23ac72ed780b6f0
Hans Vorsteveld <hvorsteveld@gmail.com>
2012-03-30 22:00:51 -0400

    Allow setVolumeFunction to enter and exit vibrate mode properly

--- src/teamwork/goodVibrations/functions/SetVolumeFunction.java
+++ src/teamwork/goodVibrations/functions/SetVolumeFunction.java
@@ -17,6 +17,7 @@ public class SetVolumeFunction extends Function
   public SetVolumeFunction(AudioManager tAM, Bundle b,int newID)
   {
     volume = b.getInt(Constants.INTENT_KEY_VOLUME);
+    Log.d(TAG,"Volume: " + volume);
     vibrate = b.getBoolean(Constants.INTENT_KEY_VIBRATE);
     name = b.getString(Constants.INTENT_KEY_NAME);
     AM = tAM;
@@ -26,7 +27,22 @@ public class SetVolumeFunction extends Function
   @Override
   public void execute()
   {
-    AM.setStreamVolume(AudioManager.STREAM_RING, volume, AudioManager.FLAG_SHOW_UI);
+    AM.setStreamVolume(AudioManager.STREAM_RING, volume, AudioManager.FLAG_SHOW_UI | AudioManager.FLAG_ALLOW_RINGER_MODES | AudioManager.FLAG_VIBRATE);
+    if(volume > 0)
+    {
+      AM.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+    }
+    if(volume == 0 && vibrate)
+    {
+      AM.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+    }
+    /*
+    // TODO Implement a silent setting
+    if(volume == 0 && silent)
+    {
+      AM.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+    }
+    */
     if(vibrate)
     {
       AM.setVibrateSetting(AudioManager.STREAM_RING,AudioManager.VIBRATE_SETTING_ON);
