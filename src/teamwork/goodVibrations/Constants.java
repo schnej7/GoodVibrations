fec93f169dce21f56714aa15d2972591fe7f9a10
zettet <tzettersten@gmail.com>
2012-03-31 23:25:02 -0400

    added map API key that I accidentally deleted

--- src/teamwork/goodVibrations/Constants.java
+++ src/teamwork/goodVibrations/Constants.java
@@ -50,6 +50,7 @@ public class Constants
   public static final String INTENT_KEY_DATA_LENGTH = "118";
   public static final String INTENT_KEY_START_FUNCTION_IDS = "119";
   public static final String INTENT_KEY_STOP_FUNCTION_IDS = "120";
+  public static final String MAP_API_KEY = "0UvdZCYV5hFgYk_rTiVtYv14xstUVf0fV2jHLhQ";
   
   // Intent request codes.  Used in onActivityResult functions to determine which activity was returned
   public static final int REQUEST_CODE_RINGTONE_PICKER = 0;
