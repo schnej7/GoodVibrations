9dce388818db3064a6f427cd2829d726cb7fd16d
zettet <tzettersten@gmail.com>
2012-04-18 11:40:42 -0400

    got rid of thing

--- src/teamwork/goodVibrations/TimeTriggerEditActivity.java
+++ src/teamwork/goodVibrations/TimeTriggerEditActivity.java
@@ -122,20 +122,3 @@ public class TimeTriggerEditActivity extends Activity
     }
   }
 }
-/*
-public class DataReceiver extends BroadcastReceiver
-{
-  @Override
-  public void onReceive(Context context, Intent intent) //PUT IT IN THE MANIFEST
-  {
-    Log.d(TAG, "RECEIVED BROADCAST MESSAGE");
-    
-    Bundle b = intent.getExtras();
-    
-    if(b.getInt(Constants.INTENT_KEY_TYPE)== Constants.TRIGGER_TYPE_TIME)
-    {
-      
-    }
-    
-  }
-}*/
